2eef67ebd1ac41cd48f4d08b49191b36838656b1
xujia <18352538350@163.com>
2026-01-16 00:03:54 +0800

    delete some useless files

--- tests/test_annotated.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-
-def say_hello(name: Annotated[str, "this is just metadata"]) -> str:
-    return f"Hello {name.__repr__()}"
-
-
-if __name__=='__main__':
-    print(say_hello("Jack"))
